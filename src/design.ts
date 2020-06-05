ee20d0ff8850b1ca92e8f8a2813e90d86598d566
hhh <1340545944@qq.com>
2020-06-05 20:53:27 +0800

    add optional default props

--- src/design.ts
+++ src/design.ts
@@ -3,9 +3,10 @@
  * (similar to `X.createElement`, except that
  * element tag is predefined and a default
  * class name will be injected if `class` property
- * doesn't exist in `props`; note that if you are
- * specifying custom class names in `props`, you'll
- * need to include the default one to make styles work.)
+ * doesn't exist in `props`; note that default
+ * class name(s) and other default props may be
+ * overwritten by your props, and if this happens,
+ * you may need to deal with it yourself.)
  */
 export type DesignComponent = (props?: object | null, ...children: unknown[]) => any;
 /** dts2md break */
@@ -13,14 +14,18 @@ export type DesignComponent = (props?: object | null, ...children: unknown[]) =>
  * Design a type of components
  * @param tag The element tag
  * @param className Default class name(s)
+ * @param defaults Optional default props
  * @returns A component factory
  */
-export const design = (tag: string, className: string | string[]): DesignComponent => (
-    (props, ...children) => {
-        const _props = {
-            class: (props && (props as any).class) || className,
-            ...props,
-        };
-        return X.createElement(tag, _props, ...children);
-    }
-);
+export const design = (
+    tag: string,
+    className: string | string[],
+    defaults?: object | null
+): DesignComponent => (props, ...children) => {
+    const _props = {
+        class: (props && (props as any).class) || className,
+        ...defaults,
+        ...props,
+    };
+    return X.createElement(tag, _props, ...children);
+};
